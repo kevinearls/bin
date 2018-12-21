34493fe32909f4d2b9d8579307c1f9981cb328ba
Kevin Earls <kearls@kevinearls.com>
2018-12-21 10:52:38 +0100

    Bring everything up to date

--- java8.sh
+++ java8.sh
@@ -1,1 +1,5 @@
-export JAVA_HOME=$(/usr/libexec/java_home)
+#:/bin/sh
+export MAVEN_OPTS="-Xmx2048m -Xms512m -XX:MaxPermSize=256m"
+export JAVA_OPTS="-Xmx2048m -Xms512m -XX:MaxPermSize=256m"
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home
+export PATH=$JAVA_HOME/bin:$PATH
