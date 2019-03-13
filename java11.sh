e5a1ca46f6a983527c3f4cd9fd81f7acf2cb7497
Kevin Earls <kearls@redhat.com>
2019-03-13 14:20:12 +0100

    Added new script for Jaeger Operator

--- java11.sh
+++ java11.sh
@@ -1,4 +1,4 @@
 #export JAVA_HOME=$(/usr/libexec/java_home)
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.1.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/
 export PATH=$JAVA_HOME/bin:$PATH
 
