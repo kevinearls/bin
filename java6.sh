1c5cf80ae2cd9f7a641499027dd3cb050f7313dd
Kevin Earls <kearls@redhat.com>
2019-03-27 10:16:52 +0100

    Updating java versions

--- java6.sh
+++
@@ -1,3 +0,0 @@
-#:/bin/sh
-export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-export PATH=$JAVA_HOME/bin:$PATH
--- java8.sh
+++ java8.sh
@@ -1,5 +1,5 @@
 #:/bin/sh
 export MAVEN_OPTS="-Xmx2048m -Xms512m -XX:MaxPermSize=256m"
 export JAVA_OPTS="-Xmx2048m -Xms512m -XX:MaxPermSize=256m"
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home
 export PATH=$JAVA_HOME/bin:$PATH
