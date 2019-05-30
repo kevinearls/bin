891f3c42b2d498d15aa102a630178b9c4ae8b16a
Kevin Earls <kearls@redhat.com>
2019-05-30 10:06:26 +0200

    Remove unneeded scripts
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- disablePatching.sh
+++
@@ -1,2 +0,0 @@
-set -x
-echo "patching.disabled = true" >> etc/system.properties
--- runMany.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-iterations=3
-tests=(JournalTest)
-tests=("${tests[@]}" PageFileTest)
-for test in "${tests[@]}"
-do
-    echo "Test is" $test
-    repeat $iterations $test
-done
